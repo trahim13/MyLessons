766948cd227792d06f1d2a677a8814a2b6236cc7
Yauheni Trakhimovich <trachimovich@gmail.com>
2017-06-11 08:41:55 +0300

    OptionalItems

--- src/RegExp/RegExp7.java
+++ src/RegExp/RegExp7.java
@@ -12,13 +12,13 @@ public class RegExp7 {
             System.out.println(m1.start() + " " + m1.group());
         }
 
-        //Pattern p2 = Pattern.compile("Get|Set|SetValue|GetValue");
+//        Pattern p2 = Pattern.compile("Get|Set|SetValue|GetValue");
 //        Pattern p2 = Pattern.compile("Get|SetValue|GetValue|Set");
 //        Pattern p2 = Pattern.compile("Get(Value)|Set(Value)");
 //        Pattern p2 = Pattern.compile("\\bGet(Value)?|Set(Value)?\\b");
         Pattern p2 = Pattern.compile("\\b(Set|Get)(Value)?\\b");
 
-        Matcher m2 = p2.matcher(" SetValue ");
+        Matcher m2 = p2.matcher("SetValue ");
         while (m2.find()) {
             System.out.println(m2.start() + " " +m2.group());
         }
--- src/RegExp/RegExp8_OptionalItems.java
+++ src/RegExp/RegExp8_OptionalItems.java
@@ -11,5 +11,19 @@ public class RegExp8_OptionalItems {
         while (m1.find()) {
             System.out.print(m1.start() + " " + m1.group() + " ");
         }
+        System.out.println();
+
+        Pattern p2 = Pattern.compile("Nov(ember)?");
+        Matcher m2 = p2.matcher("November Nov");
+        while (m2.find()) {
+            System.out.print(m2.start()+" "+m2.group()+" ");
+        }
+        System.out.println();
+
+        Pattern p3 = Pattern.compile("Feb(ruary)? 23(rd)?");
+        Matcher m3 = p3.matcher("February 23rd, February 23, Feb 23rd, Feb 23 ");
+        while (m3.find()) {
+            System.out.print(m3.group() + " ");
+        }
     }
 }
